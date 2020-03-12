19fcde3850336397818b64635bdd1ab35ad5e049
Tam Lu <lctam@tma.com.vn>
2020-03-12 16:02:09 +0700

    tslint :wq

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from "./components/main/main.component";
-import { LoginMainComponent } from "../login/components/main/main.component";
-import {CheckoutComponent} from "./components/checkout/checkout.component";
+import { MainComponent } from './components/main/main.component';
+import { LoginMainComponent } from '../login/components/main/main.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
 
 
 const routes: Routes = [
